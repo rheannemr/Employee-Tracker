a38ea0632733040bce10ef3bdd3ec6c8067285c2
Rheanne Reyes <rheannemr@gmail.com>
2021-02-11 18:28:29 -0800

    Add appMenu prompt

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS department_db;
 
 CREATE DATABASE department_db;
 
-USE DATABASE department_db;
+USE department_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
@@ -10,11 +10,12 @@ CREATE TABLE department (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE roles (
+CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(50) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id),
     PRIMARY KEY (id)
 );
 
@@ -23,6 +24,8 @@ CREATE TABLE employees (
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NOT NULL
+    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id),
     PRIMARY KEY (id)
 );
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+// Install dependencies
 const inquirer = require('inquirer');
 const mysql = require('mysql');
+const cTable = require('console.table');
 
+// Create connection 
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -8,3 +11,28 @@ const connection = mysql.createConnection({
     password: '',
     database: "department_db"
   });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected as Id" + connection.threadId);
+    appMenu();
+});
+
+function appMenu {
+    inquirer.prompt([
+     {
+         type: "list",
+         message: "Hello! What would you like to do?",
+         name: "choice",
+         choices: [
+             "View all departments",
+             "View all roles",
+             "View all employees",
+             "Add a department",
+             "Add a role",
+             "Add an employee",
+             "Update employee information"
+         ]
+     }   
+    ])
+}
